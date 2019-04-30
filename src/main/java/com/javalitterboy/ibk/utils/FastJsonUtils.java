93407f940d8a2968438a476d933d5b687858e0da
JavaLitterBoy <javalitterboy@gmail.com>
2019-04-30 10:52:43 +0800

    修改打包插件

--- src/main/java/com/javalitterboy/ibk/utils/FastJsonUtils.java
+++ src/main/java/com/javalitterboy/ibk/utils/FastJsonUtils.java
@@ -11,21 +11,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author 14183
+ */
 public class FastJsonUtils {
     private static final SerializeConfig config;
 
     static {
         config = new SerializeConfig();
-        config.put(java.util.Date.class, new JSONLibDataFormatSerializer()); // 使用和json-lib兼容的日期输出格式
-        config.put(java.sql.Date.class, new JSONLibDataFormatSerializer()); // 使用和json-lib兼容的日期输出格式
+        // 使用和json-lib兼容的日期输出格式
+        config.put(java.util.Date.class, new JSONLibDataFormatSerializer());
+        // 使用和json-lib兼容的日期输出格式
+        config.put(java.sql.Date.class, new JSONLibDataFormatSerializer());
     }
 
     private static final SerializerFeature[] features = {
-            SerializerFeature.WriteMapNullValue, // 输出空置字段
-            SerializerFeature.WriteNullListAsEmpty, // list字段如果为null，输出为[]，而不是null
-            SerializerFeature.WriteNullNumberAsZero, // 数值字段如果为null，输出为0，而不是null
-            SerializerFeature.WriteNullBooleanAsFalse, // Boolean字段如果为null，输出为false，而不是null
-            SerializerFeature.WriteNullStringAsEmpty // 字符类型字段如果为null，输出为""，而不是null
+            // 输出空置字段
+            SerializerFeature.WriteMapNullValue,
+            // list字段如果为null，输出为[]，而不是null
+            SerializerFeature.WriteNullListAsEmpty,
+            // 数值字段如果为null，输出为0，而不是null
+            SerializerFeature.WriteNullNumberAsZero,
+            // Boolean字段如果为null，输出为false，而不是null
+            SerializerFeature.WriteNullBooleanAsFalse,
+            // 字符类型字段如果为null，输出为""，而不是null
+            SerializerFeature.WriteNullStringAsEmpty
     };
 
 
